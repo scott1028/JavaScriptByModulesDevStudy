18dc72ba66694654a967539e6aa91bdbc2d74cd0
scott <mic1028002@gmail.com>
2015-08-18 22:08:31 +0800

    update code

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 console.log('Hello World at main.js!');
 
 
+//
 require(["utils/toolkit", "utils/others", "utils/nested"], function(util, others, nested) {
     // This function is called when scripts/helper/util.js is loaded.
     // If util.js calls define(), then this function is not fired until
