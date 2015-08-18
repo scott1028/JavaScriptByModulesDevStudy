fe923fac7eb54b12643449c3f5bac5048c9ebe0f
scott.lan <scott.lan@taisys.com>
2015-08-18 19:57:06 +0800

    update plugins

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,63 @@
 'use strict';
 
 module.exports = function(grunt) {
+    
+
+    //
+    var staticFileMiddleware = function(req, res, next) {
+        var serveStatic = require('serve-static');
+        var serve = serveStatic('./');
+        serve(req, res);
+    };
+
+
+    //
+    var allMiddlewares = function(connect, options) {
+        var optBase = (typeof options.base === 'string') ? [options.base] : options.base;
+        return [
+            require('connect-modrewrite')(require('./.htaccess.js').rewrite),   // rewriteRule support
+            staticFileMiddleware    // staticFile serve
+        ]
+    }
+
+
+    //
     grunt.loadNpmTasks('grunt-wiredep');
+
+
+    //
     grunt.initConfig({
         wiredep: {
-
             task: {
                 src: [
                     '*.html', // .html support...
                 ],
             }
+        },
+        connect: {
+            options: {
+                // base: '.',
+                port: 9000,
+                hostname: 'localhost',
+                keepalive: true,    // 維持伺服器開啟
+            },
+            development: {
+                options: {
+                    middleware: allMiddlewares
+                }
+            }
         }
+    });
+
+
+    //
+    grunt.loadNpmTasks('grunt-contrib-connect');
+
+
+    //
+    grunt.registerTask('lift', function (target) {
+        grunt.task.run([
+            'connect:development'
+        ]);
     });
 };
